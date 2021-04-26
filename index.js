ddd67f1260e3fcac04e05147a2e917fd4203f9f1
sajagshrestha <sajagshrestha0852@gmail.com>
2021-04-26 15:01:31 +0545

    added hamburger menu

---
+++ index.js
@@ -0,0 +1,7 @@
+const hamburger = document.querySelector(".hamburger");
+
+const mobileMenu = document.querySelector(".mobile-menu");
+
+hamburger.addEventListener("click", () => {
+	mobileMenu.classList.toggle("show");
+});
